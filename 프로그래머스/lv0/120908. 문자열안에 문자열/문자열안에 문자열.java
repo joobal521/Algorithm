6db94a276c49ec10fe91f41f28ebffc9295ba044
주바리 <128302269+joobal521@users.noreply.github.com>
2023-09-25 18:53:51 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,14 +1,25 @@
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        //contains메소드: 주어진 문자열이 단순히 포함되어 있는지 조사
-               if(str1.contains(str2)){
-                answer=1;   
-               }else{
-                   answer=2;
-               }
-           
-        
-        return answer;
+        int len1 = str1.length();
+        int len2 = str2.length();
+        boolean isMatch = false;
+
+        for (int i = 0; i <= len1 - len2; i++) {
+            if (str1.charAt(i) == str2.charAt(0)) {
+                isMatch = true;
+                for (int j = 1; j < len2; j++) {
+                    if (str1.charAt(i + j) != str2.charAt(j)) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch) {
+                    return 1;
+                }
+            }
+        }
+
+        return 2;
     }
 }
