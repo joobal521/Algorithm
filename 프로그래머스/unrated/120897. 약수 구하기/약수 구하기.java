c939cfc30028b8089a32f67c351984577628a1e3
주바리 <128302269+joobal521@users.noreply.github.com>
2023-10-16 15:09:00 +0900

    [level unrated] Title: 약수 구하기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        int count=0;
+       
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        answer=new int[count];
+        
+        int index=0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                answer[index++]=i;
+            }
+        }
+        
+        return answer;
+    }
+}
