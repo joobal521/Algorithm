73df410f6c6a4ab98c61a55cdad2536ab00387fa
주바리 <128302269+joobal521@users.noreply.github.com>
2023-10-17 19:52:54 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -1,16 +1,23 @@
 class Solution {
     public int solution(int[] num_list) {
-        int oddSum = 0;
-        int evenSum = 0;
+        int answer=0;
+        int odd = 0;
+        int even = 0;
 
         for (int i = 0; i < num_list.length; i++) {
             if (i % 2 == 0) {
-                oddSum += num_list[i];
+                 even+= num_list[i];
             } else {
-                evenSum += num_list[i];
+                odd += num_list[i];
             }
         }
+        
+        if(even>=odd){
+            answer=even;
+        }else{
+            answer=odd;
+        }
 
-        return Math.max(oddSum, evenSum);
+        return answer;
     }
 }
