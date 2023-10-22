0b1b37dc42b27e2f6b8e562715cc262f7b62e82b
주바리 <128302269+joobal521@users.noreply.github.com>
2023-10-22 13:20:58 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,20 +1,14 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-
-        // a와 b를 오름차순으로 정렬
-        int min = Math.min(a, b);
-        int max = Math.max(a, b);
-
-        if (min % 2 == 1 && max % 2 == 1) {
-            answer = min * min + max * max;
-        } else if (min % 2 == 1 || max % 2 == 1) {
-            answer = 2 * (min + max);
-        } else {
-            answer = max - min;
-        }
-
         
+        if(a%2==1 && b%2==1){
+            answer=(a*a)+(b*b);
+        }else if(a%2==1 || b%2==1){
+            answer=2*(a+b);
+        }else if(a%2==0 && b%2==0){
+            answer=Math.abs(a-b);//절댓값 
+        }
         
         return answer;
     }
