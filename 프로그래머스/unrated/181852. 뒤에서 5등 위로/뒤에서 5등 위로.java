0b4b69ce17a53c826e28a6fd09ae6161b1a4d9c3
주바리 <128302269+joobal521@users.noreply.github.com>
2023-11-06 21:52:09 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 0.33 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        
+        answer=new int[num_list.length-5];
+        Arrays.sort(num_list);
+        
+        int index=0;
+        for(int i=5; i<num_list.length; i++){
+            answer[index++]=num_list[i];
+        }
+        
+        
+        
+        return answer;
+    }
+}
