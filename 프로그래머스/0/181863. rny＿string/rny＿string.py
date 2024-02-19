ccd8bd46b16e7afc04cc0d992f83926bd0a61c7c
주바리 <128302269+joobal521@users.noreply.github.com>
2024-02-19 14:28:51 +0900

    [level 0] Title: rny_string, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,10 @@
+def solution(rny_string):
+    answer = ''
+    
+    for char in rny_string:
+        if char == 'm':
+            answer += 'rn'
+        else:
+            answer += char
+    
+    return answer
